b4e6938db3f2591ad88baff9a95296df6d0b9f6a
Sasha Marchuk <Sasha.Marchuk.email@gmail.com>
2022-06-29 01:18:43 +0300

    Add html structure for header view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './Header.scss';
+import logo from '../../assets/rocket.png';
 
-const Header = () => <div className="Header">HEADER</div>;
+import './Header.scss';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <div className="Header">
+    <div className="Header__logo">
+      <img src={logo} alt="Logo" className="Header__logo__icon" />
+    </div>
+    <nav className="Header__nav">
+      <ul className="Header__nav__list">
+        <li className="Header__nav__list__item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="Header__nav__list__item">
+          <Link to="/game">Game</Link>
+        </li>
+        <li className="Header__nav__list__item">
+          <Link to="/todo">ToDo</Link>
+        </li>
+      </ul>
+      <div className="Header__nav__login">
+        <Link to="/">Login</Link>
+      </div>
+    </nav>
+  </div>
+);
 
 Header.propTypes = {};
 
