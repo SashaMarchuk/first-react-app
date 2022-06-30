987c0a909637526d92791185a8c6e4e65e6dbbdd
Sasha Marchuk <Sasha.Marchuk.email@gmail.com>
2022-06-30 18:43:33 +0300

    Remove console.log

--- src/firebase/fireStore/methods.js
+++ src/firebase/fireStore/methods.js
@@ -3,9 +3,7 @@ import { db as dataBase } from './fireStore';
 
 const getContainers = async (db = dataBase) => {
   const containersCol = collection(db, 'containers');
-  console.log('containersCol: ', containersCol);
   const containersSnapshot = await getDocs(containersCol);
-  console.log('containersSnapshot: ', containersSnapshot);
   const containersList = containersSnapshot.docs.map((doc) => doc.data());
   return containersList;
 };
